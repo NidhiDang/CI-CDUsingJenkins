e675650341538a2001d5d0efc28942f73ac29ac0
NidhiDang <nidhi.pal@cloudanalogy.com>
2026-03-29 19:20:39 +0530

    configuration changes added

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-    reporter: [
-      ['list'],
-      ['allure-playwright']
-    ],
+  reporter: [
+  ['list'],
+  ['allure-playwright', { outputFolder: 'allure-results' }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
